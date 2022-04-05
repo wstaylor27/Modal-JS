00e6449b9665bfaa62bf617bbb1a8191bc5adb9f
Zuke27 <wstaylor27@gmail.com>
2022-04-04 22:43:19 -0400

    Initiated js logic

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+const dateElement = document.getElementById("date");
+const saveElements = document.getElementsByClassName("save");
+const contentElements = document.getElementsByClassName("content");
