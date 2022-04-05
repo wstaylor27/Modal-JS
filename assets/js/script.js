6086bacc1fa6febfa1b3093a1aa13ffe0c8d90d5
Zuke27 <wstaylor27@gmail.com>
2022-04-04 22:56:17 -0400

    Updated dom elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,13 @@
 const dateElement = document.getElementById("date");
 const saveElements = document.getElementsByClassName("save");
 const contentElements = document.getElementsByClassName("content");
+
+//save reference to important DOM elements
+var timeDisplayEl = S("#time-display");
+var projectDisplayEl = $("#project-display");
+var projectModalEl = $("#project-modal");
+var projectFormEl = $("#project-form");
+var projectNameInputEl = $("#project-name-input");
+var projectTypeInputEl = $("#project-type-input");
+var hourlyRateInputEl = $("#hourly-rate-input");
+var dueDateInputEl = $("#due-date-input");
