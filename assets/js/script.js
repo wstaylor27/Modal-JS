da5828ddfbff947c04a391e6996e1f14e59e4b4f
Zuke27 <wstaylor27@gmail.com>
2022-04-05 23:04:33 -0400

    Pushing project data to the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,4 +18,16 @@ function displayTime() {
   timeDisplayEl.text(rightNow);
 }
 
-//
+// handle printing project data to the page
+function printProjectData(name, type, hourlyRate, dueDate) {
+  var projectRowEl = $("<tr>");
+
+  var projectNameTdEl = $("<td>").addClass("p-2").text(name);
+  var projectTypeTdEl = $("<td>").addClass("p-2").text(type);
+  var rateTdEl = $("<td>").addClass("p-2").text(hourlyRate);
+  var dueDateTdEl = $("<td>").addClass("p-2").text(dueDate);
+  var daysToDate = moment(dueDate, "MM/DD/YYYY").diff(moment(), "days");
+  var daysLeftTdEl = $("<td>").addClass("p-2").text(daysToDate);
+
+  var totalEarnings = calculateTotalEarnings(hourlyRate, daysToDate);
+}
